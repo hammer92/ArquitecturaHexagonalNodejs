06fe867b352b5cd91b6a683c7493e8c86d2d4313
Hammer Pinto <profesionalsistemas3@pharmasan.co>
2020-12-28 14:17:14 -0500

    implementacion singleton sequilize

--- src/loaders/sequelize.js
+++ src/loaders/sequelize.js
@@ -4,31 +4,51 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/database')[env];
-const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-const dirModels = path.join(__dirname, '../', 'database','models')
-fs.readdirSync(dirModels)
-    .filter(file => {
-        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-    })
-    .forEach(file => {
-        const model = require(path.join(dirModels, file))(sequelize, Sequelize.DataTypes);
-        db[model.name] = model;
-    });
+class SequelizeSingleton {
 
-Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
+    constructor() {
+        if (SequelizeSingleton.instance === 'object'){
+            return SequelizeSingleton.instance
+        }
+
+        this.Sequelize = Sequelize
+
+        this.newSequelize()
+
+        this.loadModels()
+
+        this.setAssociate()
+
+        SequelizeSingleton.instance = this
+
+        return this
     }
-});
+    newSequelize(){
+        if (config.use_env_variable) {
+            this.sequelize = new Sequelize(process.env[config.use_env_variable], config);
+        } else {
+            this.sequelize = new Sequelize(config.database, config.username, config.password, config);
+        }
+    }
+    loadModels(){
+        const dirModels = path.join(__dirname, '../', 'database','models')
+        fs.readdirSync(dirModels)
+            .filter(file => {
+                return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+            })
+            .forEach(file => {
+                const model = require(path.join(dirModels, file))(this.sequelize, Sequelize.DataTypes);
+                this[model.name] = model;
+            });
+    }
+    setAssociate(){
+        Object.keys(this).forEach(modelName => {
+            if (this[modelName].associate) {
+                this[modelName].associate(this);
+            }
+        });
+    }
+}
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = new SequelizeSingleton();
