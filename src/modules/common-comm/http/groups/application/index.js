dca54401424998eb0131e3ee4da70598418e52e4
Hammer Pinto <profesionalsistemas3@pharmasan.co>
2021-01-06 18:30:48 -0500

    se agrega el modulo common

---
+++ src/modules/common-comm/http/groups/application/index.js
@@ -0,0 +1,6 @@
+// const getCase = require('./get')
+module.exports = ({ repository }, base) => {
+  return {
+    // getCase: getCase({ repository }, base),
+  }
+}
---
+++ src/modules/common-comm/router.js
@@ -0,0 +1,7 @@
+// const groups_router = require('./http/groups/router')
+const { Router } = require('express')
+const router = Router()
+module.exports = ({ application, response, logger }) => {
+  // router.use('/groups',groups_router({ application, response, logger}) )
+  return router
+}
